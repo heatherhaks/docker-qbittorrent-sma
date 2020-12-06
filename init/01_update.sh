c76a974723768d458aeec1f41315039d37f066ca
Heather Kiefer <heatherannekiefer@gmail.com>
2020-12-06 13:17:24 -0700

    Update 01_update.sh

--- init/01_update.sh
+++ init/01_update.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/with-contenv bash
 echo "updating packages"
 apt-get update && apt-get upgrade
-echo "grabbing latest mp4automator"
-[[ ! -d /mp4automator/.git ]] && (git clone git://github.com/mdhiggins/sickbeard_mp4_automator.git /mp4automator && chown -R abc:abc /mp4automator)
-echo "setting up mp4Automator"
-[[ ! -f /mp4automator/autoProcess.ini ]] && (cp /mp4automator/autoProcess.ini.sample /mp4automator/autoProcess.ini)
-
-echo "updating permissions"
-chmod 775 -R /mp4automator
 
 echo "installing pip packages"
 pip install --upgrade PIP
